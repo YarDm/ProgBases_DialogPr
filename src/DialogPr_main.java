98839318d88d5eb14ddbcef9810231d771ad5d25
Ярослав <y.o.dmitriv@gmail.com>
2018-06-15 23:11:18 +0300

    FXML file fixed. UI from FXML builded.

---
+++ src/DialogPr_main.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class DialogPr_main extends Application {
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/controller/homework.fxml"));
+            var scene = new Scene(root);
+            this.primaryStage.setTitle("Отчетное задание по Основам программирования");
+            this.primaryStage.setScene(scene);
+            this.primaryStage.show();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+}
