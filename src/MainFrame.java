9c6b4e56da9a6877c6305f10adacd36710da2100
Ярослав <y.o.dmitriv@gmail.com>
2018-07-17 21:35:59 +0300

    Complete JTable filling.

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,80 +1,72 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
 
-    TableData tableData;
+    private TableData tableData;
 
-    JTextField colQuantity;
-    JTextField rowQuantity;
-    JLabel catName;
-    JButton okButton;
-    NewTableModel tableModel;
-    JTable table;
+    private JTextField rowQuantity;
+    private JTextField colQuantity;
+    private JButton okButton;
+    private NewTableModel tableModel;
+    private JTable table;
 
-    JLabel fillId;
-    JLabel randFill;
-    JTextField fromField;
-    JLabel t;
-    JTextField toField;
-    JButton fillButton;
+    private JTextField fromField;
+    private JTextField toField;
 
-    JLabel valToCell;
-    JLabel value;
-    JTextField valueFld;
-    JTextField vCol;
-    JTextField vRow;
-    JButton vFillButton;
+    private JTextField valueFld;
+    private JTextField vCol;
+    private JTextField vRow;
 
 
-    public MainFrame(){
+    private MainFrame(){
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setName("Main frame");
         setLocation(50, 50);
         setPreferredSize(new Dimension(800, 600));
         setResizable(false);
 
-        catName = new JLabel("Задайте количество столбцов и нажмите ОК");
+        JLabel catName = new JLabel("Задайте количество столбцов и нажмите ОК");
         catName.setPreferredSize(new Dimension(350,30));
         catName.setHorizontalAlignment((int) CENTER_ALIGNMENT);
-        colQuantity = new JTextField();
-        colQuantity.setToolTipText("col");
-        colQuantity.setPreferredSize(new Dimension(60, 35));
         rowQuantity = new JTextField();
         rowQuantity.setToolTipText("row");
         rowQuantity.setPreferredSize(new Dimension(60, 35));
+        colQuantity = new JTextField();
+        colQuantity.setToolTipText("col");
+        colQuantity.setPreferredSize(new Dimension(60, 35));
         okButton = new JButton("OK");
         okButton.setPreferredSize(new Dimension(60, 35));
 
         table = new JTable(tableModel);
-        table.setRowHeight(20);
-        JScrollPane scrollPane = new JScrollPane(table);
+        table.setRowHeight(35);
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        JScrollPane scrollPane = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setPreferredSize(new Dimension(380,270));
 
-        table.setPreferredSize(new Dimension(380,500));
 
-        fillId = new JLabel("Настройки заполнения созданной таблицы");
+        JLabel fillId = new JLabel("Настройки заполнения созданной таблицы");
         fillId.setPreferredSize(new Dimension(370, 25));
         fillId.setHorizontalAlignment(SwingConstants.CENTER);
         fillId.setFont(new Font("TimesNewRoman", Font.PLAIN, 15));
-        randFill = new JLabel("Заоплнение рандомными значениями из диапазона:");
+        JLabel randFill = new JLabel("Заоплнение рандомными значениями из диапазона:");
         randFill.setPreferredSize(new Dimension(370, 15));
         randFill.setHorizontalAlignment(SwingConstants.CENTER);
         fromField = new JTextField();
         fromField.setPreferredSize(new Dimension(60,35));
-        t = new JLabel("-");
+        JLabel t = new JLabel("-");
         t.setHorizontalAlignment(SwingConstants.CENTER);
         t.setPreferredSize(new Dimension(60,35));
         toField = new JTextField();
         toField.setPreferredSize(new Dimension(60,35));
-        fillButton = new JButton("Fill");
+        JButton fillButton = new JButton("Fill");
         fillButton.setPreferredSize(new Dimension(60,35));
 
-        valToCell = new JLabel("Ввести значение в заданную ячейку:");
+        JLabel valToCell = new JLabel("Ввести значение в заданную ячейку:");
         valToCell.setPreferredSize(new Dimension(370, 25));
         valToCell.setHorizontalAlignment(SwingConstants.CENTER);
-        value = new JLabel("Value:");
+        JLabel value = new JLabel("Value:");
         valueFld = new JTextField();
         valueFld.setToolTipText("value");
         valueFld.setPreferredSize(new Dimension(60,35));
@@ -84,61 +76,47 @@ public class MainFrame extends JFrame {
         vCol = new JTextField();
         vCol.setToolTipText("to col");
         vCol.setPreferredSize(new Dimension(60,35));
-        vFillButton = new JButton("Set");
+        JButton vFillButton = new JButton("Set");
         vFillButton.setPreferredSize(new Dimension(60,35));
 
 
-        okButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int cols = Integer.parseInt(colQuantity.getText());
-                int rows = Integer.parseInt(rowQuantity.getText());
+        okButton.addActionListener(e -> {
+            int cols = Integer.parseInt(rowQuantity.getText());
+            int rows = Integer.parseInt(colQuantity.getText());
 
-                okButton.setEnabled(false);
+            okButton.setEnabled(false);
 
-                tableData = new TableData(cols, rows);
+            tableData = new TableData(cols, rows);
 
 
-                tableModel = new NewTableModel(tableData);
-                table.setModel(tableModel);
+            tableModel = new NewTableModel(tableData);
+            table.setModel(tableModel);
 
-//                table.setValueAt(5, 3,5);
-//                System.out.print(table.getValueAt(3,5));
-
-
-
-            }
         });
 
-        fillButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int fromDigit = Integer.parseInt(fromField.getText());
-                int toDigit = Integer.parseInt(toField.getText());
+        fillButton.addActionListener(e -> {
+            int fromDigit = Integer.parseInt(fromField.getText());
+            int toDigit = Integer.parseInt(toField.getText());
 
-                int rows = tableData.getRows();
-                int cols = tableData.getCols();
+            int rows = tableData.getRows();
+            int cols = tableData.getCols();
 
-                for (int i = 0; i < rows; i++) {
-                    for (int j = 0; j < cols; j++) {
-                        int digit = fromDigit + (int) (Math.random() * ((toDigit - fromDigit) + 1));
-                        table.setValueAt(digit, i, j);
-                    }
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    int digit = fromDigit + (int) (Math.random() * ((toDigit - fromDigit) + 1));
+                    table.setValueAt(digit, i, j);
                 }
             }
         });
 
-        vFillButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int val = Integer.parseInt(valueFld.getText());
-                int row = Integer.parseInt(vRow.getText());
-                int col = Integer.parseInt(vCol.getText());
+        vFillButton.addActionListener(e -> {
+            int val = Integer.parseInt(valueFld.getText());
+            int row = Integer.parseInt(vRow.getText());
+            int col = Integer.parseInt(vCol.getText());
 
-                table.setValueAt(val, row, col);
-                table.updateUI();
-                System.out.println(table.getValueAt(row, col));
-            }
+            table.setValueAt(val, row, col);
+            table.updateUI();
+            System.out.println(table.getValueAt(row, col));
         });
 
 
@@ -147,10 +125,10 @@ public class MainFrame extends JFrame {
         westSide.setPreferredSize(new Dimension(400, 350));
         westSide.setMaximumSize(new Dimension(400, 350));
         westSide.add(catName);
-        westSide.add(colQuantity);
         westSide.add(rowQuantity);
+        westSide.add(colQuantity);
         westSide.add(okButton);
-        westSide.add(table);
+        westSide.add(scrollPane);
 
         JPanel eastSide = new JPanel(new FlowLayout(FlowLayout.CENTER));
         eastSide.setPreferredSize(new Dimension(400, 350));
